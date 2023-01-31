769626728d10015c7909e1ad42a248a643df8348
alejandropmz <alejandropacheco2497@gmail.com>
2023-01-30 21:22:47 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { ImageShow } from "./componets/ImageShow";
 import { Quotes } from "./componets/Quotes";
-import { Conditional } from "./Conditional";
-import { Counter } from "./Counter";
-import { Error } from "./Error";
-import { Product } from "./Product";
-
-
-
 
 function App() {
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <Conditional /> */}
-      {/* <Error /> */}
-      {/* <Product name={"alejandro"}/><hr></hr>
-      <ImageShow /> */}
       <Quotes />
     </div>
   );
