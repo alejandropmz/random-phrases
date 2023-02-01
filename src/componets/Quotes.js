cf3f84fefc6beeaa39713bd5180536e48f4c05a4
alejandropmz <alejandropacheco2497@gmail.com>
2023-01-31 20:44:32 -0500

    all changes

--- src/componets/Quotes.js
+++ src/componets/Quotes.js
@@ -18,14 +18,14 @@ export const Quotes = () => {
     <div className='container'>
       <div className='card-container'>
         <div className='quote'>
-          <h3>"<strong>{randomPhrase.quote}</strong>"</h3>
+          <p>"<strong>{randomPhrase.quote}</strong>"</p>
         </div>
         <hr className='hr' />
         <div className='author-container'>
           <div className='author'>
-            <p>{randomPhrase.author}</p>
+            <h4>{randomPhrase.author}</h4>
           </div>
-          <div className='button-container'>
+          <div>
             <button className='button-container'>
               <svg className='change-button' onClick={changePhrase} xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="green" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                 <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
