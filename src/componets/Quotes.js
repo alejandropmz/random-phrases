063caeeede70daf51008ba6e9cb906b533af783d
alejandropmz <alejandropacheco2497@gmail.com>
2023-01-31 19:52:09 -0500

    chages generated

--- src/componets/Quotes.js
+++ src/componets/Quotes.js
@@ -17,13 +17,21 @@ export const Quotes = () => {
   return (
     <div className='container'>
       <div className='card-container'>
-        <h2>"{randomPhrase.quote}"</h2>
+        <div className='quote'>
+          <h3>"<strong>{randomPhrase.quote}</strong>"</h3>
+        </div>
         <hr className='hr' />
-        <h4>{randomPhrase.author}</h4>
-        <div className='button-container'>
-          <svg className='change-button' onClick={changePhrase} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="green" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-          </svg>
+        <div className='author-container'>
+          <div className='author'>
+            <p>{randomPhrase.author}</p>
+          </div>
+          <div className='button-container'>
+            <button className='button-container'>
+              <svg className='change-button' onClick={changePhrase} xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="green" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
